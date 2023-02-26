a7f15f0631f01c45ca0b114a2535c5735067b949
jgrissom <jgrissom91@gmail.com>
2023-02-26 13:50:11 -0600

    [2.04] Perform custom validation
    - Add model error if blog name is not unique

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,12 +15,16 @@ public class HomeController : Controller
   {
     if (ModelState.IsValid)
     {
-      _dataContext.AddBlog(model);
-      return RedirectToAction("Index");
+      if (_dataContext.Blogs.Any(b => b.Name == model.Name))
+      {
+        ModelState.AddModelError("", "Name must be unique");
+      }
+      else
+      {
+        _dataContext.AddBlog(model);
+        return RedirectToAction("Index");
+      }
     }
-    else
-    {
-      return View();
-    }
+    return View();
   }
 }
